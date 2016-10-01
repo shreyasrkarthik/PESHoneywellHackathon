b75332ba869b7ce4d9c9dd4a2d3772f6ce6b1397
srk <shreyas.rkarthik@gmail.com>
2016-10-01 20:28:03 +0530

    Server code modified for adding to database

--- Python_Scripts/Server.py
+++ Python_Scripts/Server.py
@@ -3,13 +3,27 @@ import sys
 import os
 import subprocess
 import threading
+import MySQLdb
 
+def insertRMAC(u_mac, router_mac):
+	conn = MySQLdb.connect(host= "localhost", user="root", passwd="crap", db="Honeywell")
+	cursor = conn.cursor()	
+	try:
+		cursor.execute("""SELECT user_name FROM umac_user_table WHERE u_mac=%s""",(u_mac))
+		for (user_name) in cursor:
+			username = user_name[0]
+		cursor.execute("""INSERT INTO user_rmac_table VALUES (%s,%s)""",(username,router_mac))
+		conn.commit()
+	except:
+		conn.rollback()
+	conn.close()
+	return 1
+
+#main
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 3070)
 print 'starting up on %s port %s' % server_address
-
 sock.bind(server_address)
-
 sock.listen(1)
 while True:
     print 'waiting for a connection'
@@ -18,4 +32,7 @@ while True:
     print connection
     while True:
         data = connection.recv(100).strip()
-        print  'received "%s"' % data
+        user_mac,router_mac = data.split(",")
+        print(user_mac,router_mac)
+        if(insertRMAC(user_mac, router_mac)==1):
+        	print("Successfully inserted <username,router_mac>")
--- insertUserMac.py
+++ insertUserMac.py
@@ -1,9 +1,15 @@
 import MySQLdb
 conn = MySQLdb.connect(host= "localhost", user="root", passwd="crap", db="Honeywell")
-x = conn.cursor()
+cursor = conn.cursor()
+u_mac = "A1:B2:C1:D2:E4:A2"#user mac
 try:
-   x.execute("""INSERT INTO user_rmac_table VALUES (%s,%s)""",("chandler","B1:C2:D1:E1:A2:M2"))
-   conn.commit()
+	#get username
+	cursor.execute("""SELECT user_name FROM umac_user_table WHERE u_mac=%s""",(u_mac))
+	print cursor[0]
+	for (user_name) in cursor:
+		username = user_name[0]
+	cursor.execute("""INSERT INTO user_rmac_table VALUES (%s,%s)""",(username,"B1:C2:D1:E1:A2:M5"))
+	conn.commit()
 except:
-   conn.rollback()
+	conn.rollback()
 conn.close()
