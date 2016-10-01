718237c0ed52cded4152d6a154eddff2a923006d
srk <shreyas.rkarthik@gmail.com>
2016-10-02 01:46:51 +0530

    handled both wlan and eth0

--- Python_Scripts/Footprint.py
+++ Python_Scripts/Footprint.py
@@ -4,10 +4,10 @@ import os
 import subprocess
 import threading
 import re
-# from apscheduler.scheduler import Scheduler
 
 s = ""
 User_MAC = ""
+iswlan = 0
 def connect():
 	global User_MAC
 	global s
@@ -22,19 +22,44 @@ def connect():
 	except Exception as inst:
 		print inst
 
-def sendMAC(): 
-	if sys.platform == 'win32': 
-		direct_output = subprocess.check_output('ipconfig | findstr "Default Gateway"', shell = True)
-		valid_ip = ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', direct_output)	
-		ARPCommand = "ARP -a "  + valid_ip[0]
-		MACAddress  = (subprocess.check_output(ARPCommand, shell=True))
-		MACAddress  = re.findall(r'(?<!-)(?:[0-9a-f]{2}[:-]){5}[0-9a-f]{2}(?!-)',MACAddress)[0].replace('-',":")
+def sendMAC():
+	global iswlan
+	iswlan = 0
+	if sys.platform == 'win32':
+		try:
+			direct_output = str(subprocess.check_output('netsh wlan show interfaces|findstr SSID', shell = True))
+			if(direct_output!=null and direct_output!=None):
+				#wlan
+				iswlan = 1
+				valid_bssid = re.findall( r'(?:[A-Fa-f0-9]{2}[:-]){5}(?:[A-Fa-f0-9]{2})', direct_output)[0]#mac
+				valid_ssid = (direct_output.split("\\")[0]).split(':')[1].strip()#wifi name
+				MACAddress = valid_ssid
+			else: 
+				#eth0
+				direct_output = str(subprocess.check_output('ipconfig | findstr "Default Gateway"', shell = True))
+				valid_ip = ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', direct_output)	
+				ARPCommand = "ARP -a "  + valid_ip[0]
+				MACAddress  = (subprocess.check_output(ARPCommand, shell=True))
+				MACAddress  = re.findall(r'(?<!-)(?:[0-9a-f]{2}[:-]){5}[0-9a-f]{2}(?!-)',MACAddress)[0].replace('-',":")							
+		except Exception, e:
+			pass
+		
+	else:
+		try:
+		 	if(str(subprocess.check_output("route | awk 'FNR == 3 {print $8}'", shell = True)).strip() == "wlan0"):
+				iswlan = 1
+				#User Essid but named as MACAddress
+				ESSID = subprocess.check_output('iwlist wlan0 scanning | grep ESSID',shell = True).strip().split()[0]
+				MACAddress = ESSID.split(':')[1]
 
-	else: 
-		# os.system("ip route list | awk 'FNR == 1 {print $3}'")
-		direct_output = subprocess.check_output("ip route list | awk 'FNR == 1 {print $3}'", shell=True)	
-		ARPCommand = "arp "  + direct_output.strip() + " | awk 'FNR == 2 {print $3}'"
-		MACAddress  = (subprocess.check_output(ARPCommand, shell=True)).strip()
+			else:
+				direct_output = str(subprocess.check_output("ip route list | awk 'FNR == 1 {print $3}'", shell=True))			
+				ARPCommand = "arp "  + direct_output.strip() + " | awk 'FNR == 2 {print $3}'"
+				MACAddress  = (subprocess.check_output(ARPCommand, shell=True)).strip()
+
+		except Exception, e:
+		 	raise e 
+		
 	global User_MAC
 	data = User_MAC + "," + MACAddress
 	try:
@@ -42,7 +67,7 @@ def sendMAC():
 		s.sendall(data)
 	except Exception as inst:
 		print inst
-	threading.Timer((60.0 * 1), sendMAC).start()
+	threading.Timer((60.0 * 1), connect).start()
 
 def getMacAddress(): 
     if sys.platform == 'win32': 
